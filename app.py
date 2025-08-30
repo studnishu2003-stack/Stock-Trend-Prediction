a912d0042897c84410e231a5afeb0726f22360b7
Nishu <studnishu2003@gmail.com>
2025-08-30 10:41:52 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+pip install pandas_datareader
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+pip install keras
+pip install yfinance
+
+
+
