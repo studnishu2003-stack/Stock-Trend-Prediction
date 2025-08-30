c3c0e6eae6605be4fc2122c1609dc9f7accdb031
Nishu <studnishu2003@gmail.com>
2025-08-30 12:02:09 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ import matplotlib.pyplot as plt
 import pandas_datareader as data
 pip install keras
 pip install yfinance
+from keras.models import 
+
+import yfinance as yf
+import pandas as pd
+
+# Define the date range
+start = '2010-01-01'
+end = '2025-07-31'
+st.title('Stock Trend Prediction')
+
+user_input = st.text_input('Enter Stock Ticker','AAPL')
+
+# Get Apple stock data using yfinance instead of data.DataReader
+df = yf.download('AAPL', start=start, end=end)
+# Describing the data
+st.subheader('Data from 2010-2025')
+st.write(df.describe())
+
+# Display the first few rows
+df.head()
+
 
 
 
